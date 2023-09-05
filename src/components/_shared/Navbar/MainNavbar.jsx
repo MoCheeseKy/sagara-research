234348560944ce7b0e7e2606bf3d055f907248eb
Your NameMoCheeseKy <rifkyking04@gmail.com>
2023-09-05 10:22:59 +0700

    .

--- src/components/_shared/Navbar/MainNavbar.jsx
+++ src/components/_shared/Navbar/MainNavbar.jsx
@@ -3,6 +3,7 @@ import CustomButton from '../CustomButton';
 import { Dropdown, Space } from 'antd';
 import Typography from '../Typography';
 import { DownOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
 export default function MainNavbar() {
   return (
@@ -10,13 +11,13 @@ export default function MainNavbar() {
       <nav className='flex fixed bg-white w-full items-center justify-between box-border py-4 px-10  shadow-[0_35px_60px_-15px_rgba(0,0,0,0.12)]'>
         <div className='flex gap-[70px]'>
           <div>
-            <a href='home'>
+            <Link to='/'>
               <img
                 src='/assets/Images/sagara-logo-bl.png'
                 className='w-[124px]'
                 alt=''
               />
-            </a>
+            </Link>
           </div>
           <div className='flex items-center gap-[30px]'>
             <Typography.MediumText text='Consult With Sagara' />
@@ -27,9 +28,9 @@ export default function MainNavbar() {
                   {
                     key: 0,
                     label: (
-                      <button>
+                      <Link to='/whitepapers/explore-whitepapers'>
                         <Typography.MediumText text='Explore Whitepapers' />
-                      </button>
+                      </Link>
                     ),
                   },
                 ],
@@ -46,17 +47,17 @@ export default function MainNavbar() {
                 items: [
                   {
                     label: (
-                      <button>
+                      <Link to='/about-us/our-teams'>
                         <Typography.MediumText text='Our Teams' />
-                      </button>
+                      </Link>
                     ),
                     key: 0,
                   },
                   {
                     label: (
-                      <button>
+                      <Link to='/about-us/sagara-research'>
                         <Typography.MediumText text='What is Sagara Research' />
-                      </button>
+                      </Link>
                     ),
                     key: 1,
                   },
@@ -70,7 +71,9 @@ export default function MainNavbar() {
             </Dropdown>
           </div>
         </div>
-        <CustomButton text='Contact Us' />
+        <Link to='/contact-us'>
+          <CustomButton text='Contact Us' />
+        </Link>
       </nav>
     </>
   );
