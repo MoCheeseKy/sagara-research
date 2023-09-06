a82f51ad0d87902d2021e95c0c3e39cd1c411b02
Ferdiansyah Gunawan <ferdiansyahbandung3@gmail.com>
2023-09-06 11:35:51 +0700

    navbar form & collapse

--- src/components/_shared/Navbar/MainNavbar.jsx
+++ src/components/_shared/Navbar/MainNavbar.jsx
@@ -1,14 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CustomButton from '../CustomButton';
-import { Dropdown, Space } from 'antd';
+import {
+  Dropdown,
+  Space,
+  Button,
+  Drawer,
+  Modal,
+  Form,
+  Input,
+  Collapse
+} from 'antd';
 import Typography from '../Typography';
 import { DownOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { BiChevronRight } from 'react-icons/bi'
 
 export default function MainNavbar() {
+  const [form] = Form.useForm()
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const initialValues = {
+    name: '',
+    email: '',
+    phone: '',
+    company: '',
+    need: '',
+  }
+
+  const collapseItem = [
+    {
+      key: 1,
+      label: <p style={{ color: 'white' }}>Consult With Sagara</p>,
+      onClick: () => setModalOpen(true),
+      style: { borderBottom: '1px solid #404040' },
+      showArrow: false,
+      collapsible: 'disabled'
+    },
+    {
+      key: 2,
+      label: <p style={{ color: 'white' }}>Whitepapers</p>,
+      children: <Link to='/whitepapers/explore-whitepapers' style={{ color: 'white' }}>Explore Whitepapers</Link>,
+      style: { borderBottom: '1px solid #404040' },
+      extra: <div className='rounded-full w-5 text-center text-white bg-[#e31937]'>1</div>
+    },
+    {
+      key: 3,
+      label: <p style={{ color: 'white' }}>About Us</p>,
+      children: <div className='flex flex-col gap-4'>
+        <Link to='/about-us/our-teams' style={{ color: 'white' }}>Our Team</Link>
+        <Link to='/about-us/sagara-research' style={{ color: 'white' }}>What is Sagara Research</Link>
+      </div>,
+      style: { borderBottom: '1px solid #404040' },
+      extra: <div className='rounded-full w-5 text-center text-white bg-[#e31937]'>2</div>
+    },
+  ]
+
+  const modalOk = () => {
+    form.submit()
+  }
+
+  const onCancel = () => {
+    setModalOpen(false)
+  }
+
+  const handleSubmit = (value) => {
+    setModalOpen(false)
+    console.log(value);
+    form.resetFields()
+  }
+
   return (
     <>
-      <nav className='flex fixed z-[999] bg-white w-full items-center justify-between box-border py-4 px-10  shadow-[0_35px_60px_-15px_rgba(0,0,0,0.12)]'>
+      <nav className='flex fixed z-[999] bg-white w-full items-center justify-between box-border py-4 px-[10px] md:px-10 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.12)]'>
         <div className='flex gap-[70px]'>
           <div>
             <Link to='/'>
@@ -19,12 +84,11 @@ export default function MainNavbar() {
               />
             </Link>
           </div>
-          <div className='flex items-center gap-[30px]'>
+          <div className='hidden md:flex items-center gap-[30px]'>
             <Typography.MediumText text='Consult With Sagara' />
             <Dropdown
               className='text-black'
               menu={{
-                
                 items: [
                   {
                     key: 0,
@@ -75,7 +139,79 @@ export default function MainNavbar() {
         <Link to='/contact-us'>
           <CustomButton text='Contact Us' />
         </Link>
+        <div className='block md:hidden'>
+          <Button style={{ borderRadius: '4px' }} size='large' onClick={() => setDrawerOpen(true)}>
+            <GiHamburgerMenu />
+          </Button>
+        </div>
       </nav>
+      <Drawer
+        title={<span style={{color: 'white'}}>Menu</span>}
+        placement='left'
+        closable={false}
+        onClose={() => setDrawerOpen(false)}
+        open={drawerOpen}
+        width={300}
+        headerStyle={{ textAlign: 'center', backgroundColor: '#262626' }}
+        bodyStyle={{ padding: '0', backgroundColor: '#2a2a2a', }}
+        style={{ color: 'white' }}
+      >
+        <div className='flex flex-col'>
+          <Collapse
+            accordion
+            ghost
+            bordered={false}
+            items={collapseItem}
+            expandIconPosition='end'
+            expandIcon={(e) => {
+              return (
+                <BiChevronRight color='white' size={24} className={`transition ${e.isActive ? 'rotate-90' : 'rotate-0'}`} />
+              )
+            }}
+            style={{ color: 'white' }}
+          />
+        </div>
+      </Drawer>
+      <Modal
+        title='Tell Us What You Need'
+        open={modalOpen}
+        destroyOnClose
+        onOk={modalOk}
+        onCancel={onCancel}
+        okText='Submit'
+        cancelText='Close'
+        cancelButtonProps={{
+          style: { backgroundColor: '#6c757d', color: 'white' }
+        }}
+        okButtonProps={{
+          style: { backgroundColor: '#a51535', color: 'white' }
+        }}
+      >
+        <div className='py-4'>
+          <Form onFinish={handleSubmit} form={form} autoComplete='off' initialValues={initialValues}>
+            <div className='flex flex-col gap-1'>
+              <Form.Item name='name' rules={[{ required: true, message: 'This field is required' }]}>
+                <Input size='large' placeholder='Name' />
+              </Form.Item>
+              <Form.Item name='email' rules={[
+                { required: true, message: 'This field is required' },
+                { type: 'email', message: 'Please input correct email' }
+              ]}>
+                <Input size='large' placeholder='Email' />
+              </Form.Item>
+              <Form.Item name='phone' rules={[{ required: true, message: 'This field is required' }]}>
+                <Input size='large' placeholder='Phone' />
+              </Form.Item>
+              <Form.Item name='company' rules={[{ required: true, message: 'This field is required' }]}>
+                <Input size='large' placeholder='Company' />
+              </Form.Item>
+              <Form.Item name='need' rules={[{ required: true, message: 'This field is required' }]}>
+                <Input.TextArea size='large' placeholder='Brefly Describe your needs or objectives' />
+              </Form.Item>
+            </div>
+          </Form>
+        </div>
+      </Modal>
     </>
   );
 }
