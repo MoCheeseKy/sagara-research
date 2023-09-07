25c96347ffe40e0a52e4c12c218a70069a98c80c
Your NameMoCheeseKy <rifkyking04@gmail.com>
2023-09-07 13:17:12 +0700

    set responsive for caarousel

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -3,35 +3,6 @@ import { Carousel } from 'antd';
 import Typography from '../_shared/Typography';
 
 export default function LandingComponent() {
-  const contentStyle = {
-    height: '650px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-  };
-
-  const RecentBlogCard = ({ title, description }) => {
-    return (
-      <>
-        <div className='pt-[15px] hover:pt-0 duration-300'>
-          <div className='h-[420px] p-6 rounded-lg shadow-lg flex flex-col justify-between bg-[url(/public/assets/Images/blog-01a.jpg)] bg-cover'>
-            <div className="bg-white border-1 rounded w-fit px-3 py-1 text-sm">Tips</div>
-            <div>
-              <Typography.MediumHeading
-                text={title}
-                className='text-white'
-              />
-              <Typography.SmallText
-                text={description}
-                className='text-white py-3'
-              />
-            </div>
-          </div>
-        </div>
-      </>
-    )
-  }
   const dummyUpcomingEventCard = [
     {
       title:
@@ -50,6 +21,28 @@ export default function LandingComponent() {
       desc: 'The comprehensive and insightful guidebook designed specifically with and for C-level executives who are leading their organizations through the process of digital transformation. Adapting business processes, implementing new technologies like artificial intelligence (AI) or cloud computing, and managing the cultural shift within the organization require strategic planning and effective leadership.',
     },
   ];
+
+  const RecentBlogCard = ({ title, description }) => {
+    return (
+      <>
+        <div className='pt-[15px] hover:pt-0 duration-300'>
+          <div className='h-[420px] p-6 rounded-lg shadow-lg flex flex-col justify-between bg-[url(/public/assets/Images/blog-01a.jpg)] bg-cover'>
+            <div className='bg-white border-1 rounded w-fit px-3 py-1 text-sm'>
+              Tips
+            </div>
+            <div>
+              <Typography.MediumHeading text={title} className='text-white' />
+              <Typography.SmallText
+                text={description}
+                className='text-white py-3'
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
   const UpcomingEventCard = ({ title, topic, speaker, date, desc }) => {
     return (
       <div>
@@ -68,32 +61,35 @@ export default function LandingComponent() {
           </div>
         </div>
       </div>
-    )
-  }
+    );
+  };
+
   return (
     <>
       {/* Intro Banner */}
-      <div className='pt-[77px]'></div>
       <Carousel autoplay>
         <div>
-          <h3 style={contentStyle}>1</h3>
+          <div className='flex h-[100vh] flex-col pt-[77px]'>
+            <div className='flex-grow bg-[#ddd]'>Content 1 here....</div>
+          </div>
         </div>
         <div>
-          <h3 style={contentStyle}>2</h3>
+          <div className='flex h-[100vh] flex-col pt-[77px]'>
+            <div className='flex-grow bg-[#ddd]'>Content 2 here....</div>
+          </div>
         </div>
         <div>
-          <h3 style={contentStyle}>3</h3>
+          <div className='flex h-[100vh] flex-col pt-[77px]'>
+            <div className='flex-grow bg-[#ddd]'>Content 3 here....</div>
+          </div>
         </div>
       </Carousel>
       {/* End Intro Banner */}
       {/* Upcoming Event */}
-      <div className='py-16 px-16 flex flex-col items-left'>
+      <div className='pt-16 px-16 flex flex-col items-left'>
         <div className='flex flex-col items-left md:w-[85%] max-w-[1080px]'>
           <div className='flex flex-col items-left text-left mb-[20px]'>
-            <Typography.LargeHeading
-              text='Upcoming Event'
-              className='mb-10'
-            />
+            <Typography.LargeHeading text='Upcoming Event' className='mb-10' />
             <div className='grid gap-[15px]'>
               {dummyUpcomingEventCard.map((dummy, indexDummy) => (
                 <React.Fragment key={indexDummy}>
@@ -111,29 +107,40 @@ export default function LandingComponent() {
       </div>
       {/* End of Upcoming Event */}
       {/* Whitepapers Carousel */}
-      <div className='pt-[77px]'></div>
       <Carousel autoplay>
         <div>
-          <h3 style={contentStyle}>1</h3>
+          <div className='flex h-[100vh] flex-col pt-[77px]'>
+            <div className='flex-grow bg-[#ddd]'>Content 1 here....</div>
+          </div>
         </div>
         <div>
-          <h3 style={contentStyle}>2</h3>
+          <div className='flex h-[100vh] flex-col pt-[77px]'>
+            <div className='flex-grow bg-[#ddd]'>Content 2 here....</div>
+          </div>
         </div>
         <div>
-          <h3 style={contentStyle}>3</h3>
+          <div className='flex h-[100vh] flex-col pt-[77px]'>
+            <div className='flex-grow bg-[#ddd]'>Content 3 here....</div>
+          </div>
         </div>
       </Carousel>
       {/* Recent Blog Posts */}
       <div className='py-16 px-16 flex flex-col items-left'>
         <div className='flex flex-col items-left text-left mb-[20px]'>
-          <Typography.LargeHeading
-            text='Recent Blog'
-            className='mb-10'
-          />
+          <Typography.LargeHeading text='Recent Blog' className='mb-10' />
           <div className='grid md:grid-cols-3 gap-[60px] items-center w-full'>
-            <RecentBlogCard title="Skills Gap Within Organization" description="It is a serious challenge to acquire and retain talent with needed expertise in emerging technologies such as AI, machine learning and cybersecurity." />
-            <RecentBlogCard title="Skills Gap Within Organization" description="It is a serious challenge to acquire and retain talent with needed expertise in emerging technologies such as AI, machine learning and cybersecurity." />
-            <RecentBlogCard title="Skills Gap Within Organization" description="It is a serious challenge to acquire and retain talent with needed expertise in emerging technologies such as AI, machine learning and cybersecurity." />
+            <RecentBlogCard
+              title='Skills Gap Within Organization'
+              description='It is a serious challenge to acquire and retain talent with needed expertise in emerging technologies such as AI, machine learning and cybersecurity.'
+            />
+            <RecentBlogCard
+              title='Skills Gap Within Organization'
+              description='It is a serious challenge to acquire and retain talent with needed expertise in emerging technologies such as AI, machine learning and cybersecurity.'
+            />
+            <RecentBlogCard
+              title='Skills Gap Within Organization'
+              description='It is a serious challenge to acquire and retain talent with needed expertise in emerging technologies such as AI, machine learning and cybersecurity.'
+            />
           </div>
         </div>
       </div>
