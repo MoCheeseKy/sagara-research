465249dec1537f9ab01a719a80235d45544607ac
Your NameMoCheeseKy <rifkyking04@gmail.com>
2023-09-07 14:05:42 +0700

    adjust footer responsive

--- src/components/_shared/Footer/MainFooter.jsx
+++ src/components/_shared/Footer/MainFooter.jsx
@@ -6,65 +6,50 @@ import { FiArrowRight } from 'react-icons/fi';
 export default function MainFooter() {
   return (
     <>
-      <div className='w-[100%] bg-[#303030]'>
-        <footer className='flex flex-col md:flex-row min-[1200px]:max-w-[1140px] mx-auto py-[40px] text-white gap-5 px-[15px]'>
-          <div className='w-full md:w-[190px] px-[15px]'>
+      <footer className='flex flex-col bg-[#303030] items-center text-white'>
+        <div className='flex flex-col gap-6 md:gap-0 flex-wrap md:flex-row py-6 px-[15px] w-full md:w-[85%] max-w-[1080px]'>
+          <div className='w-full md:w-[40%] lg:w-[25%] px-[15px]'>
             <Typography.LargeText text='JAKARTA' className='mb-[15px]' />
             <Typography.MediumText
               text='Wisma KGBI Jl.Jend.Sudirman Kav.28 Bendungan Hilir, Kecamatan Tanah Abang, Jakarta Pusat, DKI Jakarta 10210'
               className='text-[#c0c0c0]'
             />
           </div>
-          <div className='w-full md:w-[190px] px-[15px]'>
+          <div className='w-full md:w-[40%] lg:w-[25%] px-[15px]'>
             <Typography.LargeText text='BANDUNG' className='mb-[15px]' />
             <Typography.MediumText
               text='Summarecon Bandung, Magna Commercial No MA03 Rancabolang, Kecamatan Gedebage, Bandung, Jawa Barat 40286'
               className='text-[#c0c0c0]'
             />
           </div>
-          <div className='w-full md:w-[190px] px-[15px]'>
+          <div className='w-full md:w-[20%] lg:w-[25%] px-[15px]'>
             <Typography.LargeText text='FOLLOW US' className='mb-[15px]' />
             <a href='https://www.linkedin.com/company/sagara-asia/mycompany/verification/'>
               <Typography.MediumText
                 text='Linkedin'
-                className='text-[#c0c0c0]'
+                className='text-[#c0c0c0] w-fit mb-[2px] hover:mb-0 hover:border-b-[2px] border-[#ddd] '
               />
             </a>
             <a href='https://www.instagram.com/sagaratechnology/'>
               <Typography.MediumText
                 text='Instagram'
-                className='text-[#c0c0c0]'
+                className='text-[#c0c0c0] w-fit mb-[2px] hover:mb-0 hover:border-b-[2px] border-[#ddd] '
               />
             </a>
             <a href='https://www.facebook.com/Sagaratechnology/'>
               <Typography.MediumText
                 text='Facebook'
-                className='text-[#c0c0c0]'
+                className='text-[#c0c0c0] w-fit mb-[2px] hover:mb-0 hover:border-b-[2px] border-[#ddd] '
               />
             </a>
-            <a href='https://www.facebook.com/sagaratech/"'>
+            <a href='https://www.facebook.com/sagaratech/'>
               <Typography.MediumText
                 text='Twitter'
-                className='text-[#c0c0c0]'
+                className='text-[#c0c0c0] w-fit mb-[2px] hover:mb-0 hover:border-b-[2px] border-[#ddd] '
               />
             </a>
           </div>
-          <div className='w-full md:w-[190px] px-[15px]'>
-            <Typography.LargeText text='ACCOUNT' className='mb-[15px]' />
-            <a href='/'>
-              <Typography.MediumText
-                text='Register/Log in'
-                className='text-[#c0c0c0]'
-              />
-            </a>
-            <a href='/'>
-              <Typography.MediumText
-                text='My Account'
-                className='text-[#c0c0c0]'
-              />
-            </a>
-          </div>
-          <div className='w-18 px-[15px]'>
+          <div className='flex-grow md:pt-6 lg:pt-0 px-[15px]'>
             <Typography.LargeText
               text='LETS KEEP IN TOUCH'
               className='mb-[15px] flex items-center gap-2'
@@ -92,15 +77,16 @@ export default function MainFooter() {
               </Button>
             </div>
           </div>
-        </footer>
-        {/* Copyrights */}
-        <div className='flex justify-center items-center bg-[#303030] border-t-[1px] border-[#484848] py-[15px]'>
-          <Typography.SmallText
-            text='© 2023 Sagara Research | All Rights Reserved.'
-            className='gap-2 my-5 text-white'
-          />
         </div>
-      </div>
+      </footer>
+
+      {/* Copyrights */}
+      <footer className='flex justify-center items-center bg-[#303030] border-t-[1px] border-[#484848] py-[15px]'>
+        <Typography.SmallText
+          text='© 2023 Sagara Research | All Rights Reserved.'
+          className='gap-2 my-5 text-white'
+        />
+      </footer>
     </>
   );
 }
