81499f8d19425f39967da7d3e5c337bffb057da6
Ferdiansyah Gunawan <ferdiansyahbandung3@gmail.com>
2023-09-11 11:15:32 +0700

    response blob

--- src/helper/api.js
+++ src/helper/api.js
@@ -1,17 +1,37 @@
 import axios from 'axios';
 
-const api = ({ url, method, body, headers }) => {
-  const config = {
+const downloadFile = (response, name = 'Whitepaper', format = 'pdf') => {
+  const time = new Date()
+  const fileName = name + '-' + time.toLocaleString('en-GB', { timeZone: 'UTC' })
+
+  const url = window.URL.createObjectURL(new Blob([response]));
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', `${fileName}.${format}`);
+  document.body.appendChild(link);
+  link.click();
+}
+
+const api = ({ url, method, body, headers, isDownload }) => {
+  let config = {
     url: `https://sagara-research-news.nikici.com/${url}`,
     method: method,
     data: body,
     headers: headers,
     timeout: 300000,
   };
+
+  if (isDownload) {
+    config = {
+      ...config,
+      responseType: 'blob',
+    }
+  }
+
   return axios(config);
 };
 
-const apiRequest = async ({ method, url, payload = {}, isForm }) => {
+const apiRequest = async ({ method, url, payload = {}, isForm, fileName, format }) => {
   let body = null;
   let headers = {
     'Content-Type': 'application/json',
@@ -37,7 +57,10 @@ const apiRequest = async ({ method, url, payload = {}, isForm }) => {
   }
 
   try {
-    const response = await api({ url, method, body, headers });
+    const response = await api({ url, method, body, headers, isDownload: isForm });
+    if (isForm) {
+      downloadFile(response.data, fileName, format)
+    }
     return response;
   } catch ({ response }) {
     throw response.data;
@@ -47,8 +70,8 @@ const apiRequest = async ({ method, url, payload = {}, isForm }) => {
 const get = ({ url }) => apiRequest({ url, method: 'get' });
 const deleteRequest = ({ url, payload }) =>
   apiRequest({ method: 'delete', url: url, payload: payload });
-const post = ({ url, payload, isForm }) =>
-  apiRequest({ method: 'post', url: url, payload: payload, isForm });
+const post = ({ url, payload, isForm, fileName, format }) =>
+  apiRequest({ method: 'post', url: url, payload: payload, isForm, fileName, format });
 const put = ({ url, payload }) =>
   apiRequest({ method: 'put', url: url, payload: payload });
 
