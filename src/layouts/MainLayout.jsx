046598198443e14d0492985e8779fc2f7c158a8d
Laukpindang <ferdiansyahbandung3@gmail.com>
2023-09-13 10:01:54 +0700

    add scroll to top

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useDocumentTitle from '../hooks/useDocumentTitle';
 import MainNavbar from '../components/_shared/Navbar/MainNavbar';
 import MainFooter from '../components/_shared/Footer/MainFooter';
+import { useLocation } from 'react-router-dom';
 
 export default function MainLayout({ title, children }) {
+  const { pathname } = useLocation()
   const titlePrefix = 'Sagara Research | ';
   useDocumentTitle(`${titlePrefix}${title}`);
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <>
       <MainNavbar />
