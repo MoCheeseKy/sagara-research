06dfd7fa4f7204012ea29c926a38ffc0e5c4ae4d
Your NameMoCheeseKy <rifkyking04@gmail.com>
2023-09-15 15:49:49 +0700

    .

--- src/components/_shared/ResearchCard/index.jsx
+++ src/components/_shared/ResearchCard/index.jsx
@@ -22,118 +22,113 @@ export default function ResearchCard({
 }) {
   return (
     <>
-      <Link to={`/research/detail/${slug}`}>
-        <div className='flex flex-col md:flex-row shadow rounded mt-[15px] hover:mt-0 mb-0 hover:mb-[15px] duration-300 overflow-hidden bg-white'>
-          <div className='p-6 md:p-[30px] flex flex-col'>
-            <div className='flex flex-col gap-2'>
-              <div className='flex gap-6'>
-                <div
-                  className='w-fit h-fit'
-                  style={{
-                    backgroundImage: `url(${DefaultCover})`,
-                    backgroundSize: 'cover',
-                  }}
-                >
-                  <img
-                    src={image ? image : DefaultCover}
-                    alt=' '
-                    className='w-[74px] md:min-w-[144px] md:max-w-[144px] aspect-[3/4] h-fit bg-cover'
+      <div className='flex flex-col md:flex-row shadow rounded mt-[15px] hover:mt-0 mb-0 hover:mb-[15px] duration-300 overflow-hidden bg-white'>
+        <div className='p-6 md:p-[30px] flex flex-col'>
+          <div className='flex flex-col gap-2'>
+            <div className='flex gap-6'>
+              <div
+                className='w-fit h-fit'
+                style={{
+                  backgroundImage: `url(${DefaultCover})`,
+                  backgroundSize: 'cover',
+                }}
+              >
+                <img
+                  src={image ? image : DefaultCover}
+                  alt=' '
+                  className='w-[74px] md:min-w-[144px] md:max-w-[144px] aspect-[3/4] h-fit bg-cover'
+                />
+              </div>
+              <div>
+                <div>
+                  <Typography.LargeText
+                    text={title ? `${title}` : '-'}
+                    bold
+                    className='mb-2'
                   />
                 </div>
+                <div className='flex flex-wrap gap-[6px]'>
+                  {topic?.map((topic, indexTopic) => (
+                    <React.Fragment key={indexTopic}>
+                      <Link
+                        to={`/research/explore-research?topic=${topic?.title}`}
+                        onClick={() =>
+                          (window.location.href = `/research/explore-research?topic=${topic?.title}`)
+                        }
+                      >
+                        <div className='border-primary border-[1px] px-4 rounded-lg w-fit'>
+                          <Typography.Custom
+                            text={topic?.title ? topic?.title : '-'}
+                            className='text-primary text-xs'
+                          />
+                        </div>
+                      </Link>
+                    </React.Fragment>
+                  ))}
+                </div>
                 <div>
-                  <div>
-                    <Typography.LargeText
-                      text={title ? `${title}` : '-'}
-                      bold
-                      className='mb-2'
+                  <Link
+                    to={`/research/explore-research?author=${author}`}
+                    onClick={() =>
+                      (window.location.href = `/research/explore-research?author=${author}`)
+                    }
+                    className='flex gap-2 mt-2 items-center text-[#808080] hover:text-blue-500'
+                  >
+                    <AiOutlineUser />
+                    <Typography.SmallText text={`${author ? author : '-'}`} />
+                  </Link>
+                  <div className='flex gap-2 text-[#808080] items-center'>
+                    <BsCalendar2Check />
+                    <Typography.SmallText text={`${date ? date : '-'}`} />
+                  </div>
+                  <div className='flex gap-2 text-[#808080] items-center'>
+                    <FiDownload />
+                    <Typography.SmallText
+                      text={`${download ? download : '-'}`}
                     />
                   </div>
-                  <div className='flex flex-wrap gap-[6px]'>
-                    {topic?.map((topic, indexTopic) => (
-                      <React.Fragment key={indexTopic}>
-                        <Link
-                          to={`/research/explore-research?topic=${topic?.title}`}
-                          onClick={() =>
-                            (window.location.href = `/research/explore-research?topic=${topic?.title}`)
-                          }
-                        >
-                          <div className='border-primary border-[1px] px-4 rounded-lg w-fit'>
-                            <Typography.Custom
-                              text={topic?.title ? topic?.title : '-'}
-                              className='text-primary text-xs'
-                            />
-                          </div>
-                        </Link>
-                      </React.Fragment>
-                    ))}
-                  </div>
-                  <div>
-                    <Link
-                      to={`/research/explore-research?author=${author}`}
-                      onClick={() =>
-                        (window.location.href = `/research/explore-research?author=${author}`)
-                      }
-                      className='flex gap-2 mt-2 items-center text-[#808080] hover:text-blue-500'
-                    >
-                      <AiOutlineUser />
-                      <Typography.SmallText text={`${author ? author : '-'}`} />
-                    </Link>
-                    <div className='flex gap-2 text-[#808080] items-center'>
-                      <BsCalendar2Check />
-                      <Typography.SmallText text={`${date ? date : '-'}`} />
-                    </div>
-                    <div className='flex gap-2 text-[#808080] items-center'>
-                      <FiDownload />
+                  <Link
+                    to={`/research/explore-research?language=${language}`}
+                    onClick={() =>
+                      (window.location.href = `/research/explore-research?language=${language}`)
+                    }
+                  >
+                    <div className='flex gap-2 mb-2 text-[#808080] hover:text-blue-500 items-center'>
+                      <HiLanguage />
                       <Typography.SmallText
-                        text={`${download ? download : '-'}`}
+                        text={`${language ? language : '-'}`}
                       />
                     </div>
-                    <Link
-                      to={`/research/explore-research?language=${language}`}
-                      onClick={() =>
-                        (window.location.href = `/research/explore-research?language=${language}`)
-                      }
-                    >
-                      <div className='flex gap-2 mb-2 text-[#808080] hover:text-blue-500 items-center'>
-                        <HiLanguage />
-                        <Typography.SmallText
-                          text={`${language ? language : '-'}`}
-                        />
-                      </div>
+                  </Link>
+                  <div className='hidden md:flex md:flex-col'>
+                    <Typography.Elipsis
+                      text={desc}
+                      ellipsis={3}
+                      className='text-[#808080]'
+                    />
+                    <Link to={`/research/detail/${slug}`}>
+                      <CustomButton
+                        text='Learn More'
+                        className='w-fit text-sm py-2'
+                      />
                     </Link>
-                    <div className='hidden md:flex md:flex-col'>
-                      <Typography.Elipsis
-                        text={desc}
-                        ellipsis={3}
-                        className='text-[#808080]'
-                      />
-                      <Link to={`/research/detail/${slug}`}>
-                        <CustomButton
-                          text='Learn More'
-                          className='w-fit text-sm py-2'
-                        />
-                      </Link>
-                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className='flex flex-col md:hidden'>
-              <Typography.Elipsis
-                text={desc}
-                ellipsis={3}
-                className='text-[#808080]'
-              />
-              <Link to={`/research/detail/${slug}`}>
-                <CustomButton
-                  text='Learn More'
-                  className='w-fit text-sm py-2'
-                />
-              </Link>
-            </div>
+          </div>
+          <div className='flex flex-col md:hidden'>
+            <Typography.Elipsis
+              text={desc}
+              ellipsis={3}
+              className='text-[#808080]'
+            />
+            <Link to={`/research/detail/${slug}`}>
+              <CustomButton text='Learn More' className='w-fit text-sm py-2' />
+            </Link>
           </div>
         </div>
-      </Link>
+      </div>
     </>
   );
 }
